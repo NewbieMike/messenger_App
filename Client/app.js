d60e1e3312220ba80654a556976322f69dfc1e25
Michał Micherda <mmicherdakr@gmail.com>
2021-08-08 13:36:36 +0200

    Add chat Admin

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,10 @@
+//OBSŁUGA SOCKET.IO
+const socket = io();
+
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('user', ({ author, content }) => addMessage(author, content));
+socket.on('userDisconnect', ({ author, content }) => addMessage(author, content));
+
 // STAŁE LOKALE
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
@@ -12,10 +19,11 @@ let userName;
 //FUNKCJE
 const login = function(event) {
     event.preventDefault();
-    if(userNameInput.value) {
+    if(userNameInput.value){
         userName = userNameInput.value;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('join', {name: userName, id: socket.id});
     } else {
         alert('Please try again...');
     }
@@ -26,24 +34,25 @@ function addMessage (author, content) {
     message.classList.add('message');
     message.classList.add('message--received');
     if(author === userName) {
-        message.classList.add('message--self');
-        message.innerHTML = `
-            <h3 class="message__author">${userName === author ? 'You' : author }</h3>
-            <div class="message__content">
-                ${content}
-            </div>
-        `;
-        messagesList.appendChild(message);
-    } 
+        message.classList.add('message--self');   
+    }
+    message.innerHTML = `
+        <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+        <div class="message__content">
+            ${content}
+        </div>
+    `;
+    messagesList.appendChild(message);
 }
 
 const sendMessage = function(event) {
     event.preventDefault();
     if(messageContentInput.value) {
         addMessage(userName, messageContentInput.value);
-        messageContentInput.value='';
+        socket.emit('message', { author: userName, content: messageContentInput.value})
+        messageContentInput.value = '';
     } else {
-        alert('Enter your message...');
+        alert('Try again!')
     }
 }
 //OBSŁUGA PRZYCISKÓW
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //IMPORTY
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 // URUCHOMIENIE EXPRESS'A
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.static(path.join(__dirname, '/client')));
 
 //TABLICA WIADOMOŚCI
 const messages = [];
+const users = [];
 
 //WPROWDZENIE LINKÓW WRAZ Z WIDOKAMI
 
@@ -18,6 +20,31 @@ app.get('*', (req, res) => {
 });
 
 //URUCHOMIENIE SERWERA NASŁUCHUJĄCEGO PORT 8000
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log('Server is running on port: 8000');
-})
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('join', (login) => {
+        console.log('New user: ' + socket.id);
+        users.push(login);
+        console.log(users);
+        socket.broadcast.emit('user', {author: 'Admin', content: '' + login.name + ' has joined the conversation!'});
+    })
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        console.log(messages);
+        socket.broadcast.emit('message', message);
+      });
+      socket.on('disconnect', (login) => { 
+        console.log('Socket ' + socket.id + ' has left');
+        users.shift(login);
+        console.log(users);
+        socket.broadcast.emit('userDisconnect', {author: 'Admin', content: '' + login.name + 'has left the conversation! '})
+    });
+    console.log('I\'ve added a listener on message and disconnect events \n');
+  });
